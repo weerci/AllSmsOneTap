28994ee53b7c74de10a424dd7e28741ed88939c7
dima <weerci@yandex.ru>
2015-03-31 14:18:29 +0700

    tmp154

--- app/src/main/java/com/production/kriate/allsmsonetap/fragments/EditSmsFragment.java
+++ app/src/main/java/com/production/kriate/allsmsonetap/fragments/EditSmsFragment.java
@@ -3,10 +3,13 @@ package com.production.kriate.allsmsonetap.fragments;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.production.kriate.allsmsonetap.db.DbSms;
+import com.production.kriate.allsmsonetap.R;
 
 /**
  * Created by dima on 27.03.2015.
@@ -27,6 +30,11 @@ public class EditSmsFragment extends Fragment{
         setHasOptionsMenu(true);
         ActionBar actionBar = getActivity().getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.edit_sms_layout, container, false);
+        return v;
     }
 
     @Override
