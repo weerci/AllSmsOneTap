035b5d22368d30ed9fcbfb3328533276628dd30d
dima <weerci@yandex.ru>
2015-03-31 17:15:45 +0700

    tmp155

--- app/src/main/java/com/production/kriate/allsmsonetap/fragments/EditSmsFragment.java
+++ app/src/main/java/com/production/kriate/allsmsonetap/fragments/EditSmsFragment.java
@@ -1,21 +1,43 @@
 package com.production.kriate.allsmsonetap.fragments;
 
 import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.production.kriate.allsmsonetap.db.DbCategory;
+import com.production.kriate.allsmsonetap.db.DbConnector;
 import com.production.kriate.allsmsonetap.db.DbSms;
 import com.production.kriate.allsmsonetap.R;
 
-/**
- * Created by dima on 27.03.2015.
- */
+import java.util.ArrayList;
+
+
 public class EditSmsFragment extends Fragment{
     public static final String EXTRA_SMS = "com.production.kriate.allsmsonetap.EditSmsFragment.EXTRA_SMS";
+    private static final int REQUEST_CONTACT = 0;
+    private DbSms mSms;
+    private EditText mTitleField, mTextField, mPhoneField;
+    private CheckBox mIsFavorite;
+    private Spinner mCategroySpinner;
+    private long mId;
+
 
     public static EditSmsFragment newInstance(DbSms ds) {
         Bundle args = new Bundle();
@@ -24,6 +46,7 @@ public class EditSmsFragment extends Fragment{
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +55,74 @@ public class EditSmsFragment extends Fragment{
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.edit_sms_layout, container, false);
-        return v;
+    public void onPrepareOptionsMenu(Menu menu) {
+        //menu.findItem(R.id.menu_item_new_template).setVisible(false);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.edit_sms_layout, container, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.sms_title_edit_text);
+        mTextField = (EditText) v.findViewById(R.id.sms_text_edit_text);
+        mIsFavorite = (CheckBox) v.findViewById(R.id.sms_is_favorite_checkbox);
+        mPhoneField = (EditText) v.findViewById(R.id.phone_number_edit_text);
+        mCategroySpinner = (Spinner)v.findViewById(R.id.sms_category_spinner);
+        mId = DbSms.EMPTY_ID;
+
+        CategroyAdapter adapter = new CategroyAdapter(getActivity(), DbConnector.newInstance(getActivity()).getCategory().selectWithEmpty());
+        adapter.setDropDownViewResource(R.layout.spinner_list_item);
+
+        mCategroySpinner.setAdapter(adapter);
+
+        if (mSms != null) {
+            mId = mSms.getId();
+            mTitleField.setText(mSms.getTitleSms());
+            mTextField.setText(mSms.getTextSms());
+            mPhoneField.setText(mSms.getPhoneNumber());
+            mIsFavorite.setChecked(mSms.getPriority() != 0);
+
+            if (mSms.getCategory() != null) {
+                DbCategory dbCategory = mSms.getCategory();
+                int i = adapter.getPosition(dbCategory);
+                mCategroySpinner.setSelection(i, false);
+            }
+
+        }
+
+        // Кнопки
+        Button saveButton = (Button) v.findViewById(R.id.butSave);
+        Button cancelButton = (Button) v.findViewById(R.id.butCancel);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbSms dbSms = new DbSms(mId, mTitleField.getText().toString(), mTextField.getText().toString(),
+                        mPhoneField.getText().toString(), checkToPriority(), (DbCategory)mCategroySpinner.getSelectedItem());
+
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_SMS, dbSms);
+                getActivity().setResult(Activity.RESULT_OK, intent);
+                getActivity().finish();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+/*
+        Button selectPhoneButton = (Button) v.findViewById(R.id.select_phone_button);
+        selectPhoneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(i, REQUEST_CONTACT);
+            }
+        });
+*/
+
+        return v;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home)
@@ -46,5 +132,69 @@ public class EditSmsFragment extends Fragment{
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_CONTACT) {
+            Uri contactUri = data.getData();
+            String[] queryFields = new String[] {
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+            };
+            Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+            int indexNumber = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            if (c.getCount() == 0) {
+                c.close();
+                return;
+            }
+
+            c.moveToFirst();
+            String suspect = c.getString(indexNumber);
+            mPhoneField.setText(suspect);
+        }
+    }
+
+    private int checkToPriority()
+    {
+        return mIsFavorite.isChecked()? 1 : 0;
+    }
+
+    private class CategroyAdapter extends ArrayAdapter<DbCategory> {
+
+        public CategroyAdapter(Context context, ArrayList<DbCategory> categories) {
+            super(context, R.layout.spinner_list_item, categories);
+        }
+
+        @Override
+        public int getPosition(DbCategory item) {
+            for (int i = 0; i < this.getCount(); i++) {
+                if(this.getItem(i).getId() == item.getId()) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCategoryView(position, convertView, parent);
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getCategoryView(position, convertView, parent);
+        }
+        public View getCategoryView(int position, View convertView, ViewGroup parent) {
+            DbCategory dbCategory = getItem(position);
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_list_item, null);
+            }
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(dbCategory.getName());
+
+            return convertView;
+        }
+
     }
 }
